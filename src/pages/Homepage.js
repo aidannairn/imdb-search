541b77e08ea23362f25070db668c6fb3c4ee8b81
aidannairn <aidan.nairn95@gmail.com>
2022-08-02 10:35:58 +1200

    feat: Use axios to request IMDB data and set the response as results list.

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,7 +1,45 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
 import Search from "../components/Search"
 import MoviesList from "../components/MoviesList"
 
 const Homepage = () => {
+  const [searchStr, setSearchStr] = useState('Shrek')
+  const [resultsList, setResultsList] = useState([])
+
+  const {
+    REACT_APP_IMDB_URL: url,
+    REACT_APP_IMDB_HOST: host,
+    REACT_APP_IMDB_API_KEY: apiKey
+  } = process.env
+
+  const options = {
+    method: 'GET',
+    url,
+    params: {
+      s: searchStr, 
+      r: 'json', 
+      page: '1'
+    },
+    headers: {
+      'X-RapidAPI-Key': apiKey,
+      'X-RapidAPI-Host': host
+    }
+  }
+
+  useEffect(() => {
+    axios.request(options)
+    .then(res => {
+      setResultsList(res.data.Search)
+    })
+    .catch(err => {
+      console.error(err)
+    })
+  }, [])
+  
+  
+  console.log(resultsList)
   return (
     <div>
       <Search />
