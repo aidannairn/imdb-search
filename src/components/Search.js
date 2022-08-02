642738c2a599f2f7edf28749ddfb8632e69ccc73
aidannairn <aidan.nairn95@gmail.com>
2022-08-02 19:33:55 +1200

    feat: Change Search component headings.

--- src/components/Search.js
+++ src/components/Search.js
@@ -16,8 +16,8 @@ const Search = ({ setSearchStr }) => {
 
   return (
     <div id="search-container">
-      <h1>Looking For A Movie?</h1>
-      <h1>Search Below.</h1>
+      <h1>Looking For Some Entertainment?</h1>
+      <h1>Browse movies, shows {'&'} games.</h1>
       <form id="search-bar" onSubmit={handleSubmit}>
         <input type="text" onChange={handleInputChange} />
         <button type='submit'>SEARCH</button>
