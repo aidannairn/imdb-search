ddf5f8560459a5692b2351f5fb42376d771eebfa
aidannairn <aidan.nairn95@gmail.com>
2022-08-02 12:48:25 +1200

    feat: Add items to navbar and style them with CSS.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,22 @@
+import '../styles/navbar.css'
+
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div id="navbar">
+      <div id="company">
+        <img src="https://cdn.pixabay.com/photo/2016/03/31/20/07/cartoon-1295516_960_720.png" alt="" />
+        <p>IMDB Search</p>
+      </div>
+      <div id="nav-options">
+        <form id="nav-search-bar">
+          <label>Search:</label>
+          <input type="text" />
+          <button type='submit'><i className="fa fa-search" aria-hidden="true"></i></button>
+        </form>
+        <a href="#">Home</a>
+        <a href="#">About</a>
+      </div>
+    </div>
   )
 }
 
