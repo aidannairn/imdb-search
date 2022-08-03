fbf89c50589dd5db659190856990bba84cb9d792
aidannairn <aidan.nairn95@gmail.com>
2022-08-04 11:35:47 +1200

    feat: Add hamburger menu for navbar links/buttons when viewing website on mobile devices.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import '../styles/navbar.css'
 
 const Navbar = ({ setSearchStr }) => {
   const [searchInput, setSearchInput] = useState('')
+  const [areNavLinksVisible, setAreNavLinksVisible] = useState(true)
   const [isSearchBarVisible, setIsSearchBarVisible] = useState(false)
 
   const handleSearchBtnClick = () => {
@@ -19,23 +20,35 @@ const Navbar = ({ setSearchStr }) => {
     setSearchStr(searchInput)
   }
 
+  const handleHamburgerClick = e => {
+    setAreNavLinksVisible(!areNavLinksVisible)
+  }
+
   return (
     <div id="navbar">
       <div id="company">
         <img src="https://cdn.pixabay.com/photo/2016/03/31/20/07/cartoon-1295516_960_720.png" alt="" />
         <p>IMDB Search</p>
       </div>
-      <div id="nav-options">
-        {isSearchBarVisible 
-          ? <form id="nav-search-bar" onSubmit={handleSubmit}>
-            <input type="text" onChange={handleInputChange} />
-            <button type='submit'><i className="fa fa-search" aria-hidden="true"></i></button>
-          </form>
-        : null}
-        <p onClick={handleSearchBtnClick} >Search</p>  
-        <a href="#">Home</a>
-        <a href="#">About</a>
-        <div id="login-btn"><p>Login</p></div>
+      {areNavLinksVisible && 
+        <div id="nav-options">
+          {isSearchBarVisible 
+            ? <form id="nav-search-bar" onSubmit={handleSubmit}>
+              <input type="text" onChange={handleInputChange} />
+              <button type='submit'><i className="fa fa-search" aria-hidden="true"></i></button>
+            </form>
+          : null}
+          <p onClick={handleSearchBtnClick} >Search</p>  
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <div id="login-btn"><p>Login</p></div>
+        </div>
+      }
+      <div id="hamburger-icon" className={areNavLinksVisible ? 'open' : ''} onClick={handleHamburgerClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
       </div>
     </div>
   )
