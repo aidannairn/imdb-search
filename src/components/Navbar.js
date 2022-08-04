7cc00951c20e4eec9ebee16d6166b277c720cd3e
aidannairn <aidan.nairn95@gmail.com>
2022-08-04 21:45:44 +1200

    feat: Animate hamburger menu.

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,33 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 import '../styles/navbar.css'
 
 const Navbar = ({ setSearchStr }) => {
   const [searchInput, setSearchInput] = useState('')
-  const [areNavLinksVisible, setAreNavLinksVisible] = useState(true)
+  const [areNavLinksVisible, setAreNavLinksVisible] = useState(false)
   const [isSearchBarVisible, setIsSearchBarVisible] = useState(false)
+  
+  const getWindowWidth = () => window.innerWidth
+
+  const [windowWidth, setWindowWidth] = useState(getWindowWidth())
+
+  useEffect(() => {
+    const handleWindowResize = () => {
+      const width = getWindowWidth()
+
+      setWindowWidth(width)
+
+      if (width >= 768) {
+        setAreNavLinksVisible(false)
+      }
+    }
+
+    window.addEventListener('resize', handleWindowResize)
+
+    return () => {
+      window.removeEventListener('resize', handleWindowResize)
+    }
+  }, [])
 
   const handleSearchBtnClick = () => {
     setIsSearchBarVisible(!isSearchBarVisible)
@@ -30,18 +52,26 @@ const Navbar = ({ setSearchStr }) => {
         <img src="https://cdn.pixabay.com/photo/2016/03/31/20/07/cartoon-1295516_960_720.png" alt="" />
         <p>IMDB Search</p>
       </div>
-      {areNavLinksVisible && 
+      {(areNavLinksVisible || (windowWidth > 768)) && 
         <div id="nav-options">
+          <div className={`nav-option ${areNavLinksVisible ? 'open' : ''}`}>
           {isSearchBarVisible 
             ? <form id="nav-search-bar" onSubmit={handleSubmit}>
               <input type="text" onChange={handleInputChange} />
               <button type='submit'><i className="fa fa-search" aria-hidden="true"></i></button>
             </form>
           : null}
-          <p onClick={handleSearchBtnClick} >Search</p>  
-          <a href="#">Home</a>
-          <a href="#">About</a>
-          <div id="login-btn"><p>Login</p></div>
+            <p onClick={handleSearchBtnClick} >Search</p>  
+          </div>
+          <div className={`nav-option ${areNavLinksVisible ? 'open' : ''}`}>
+            <a href="#">Home</a>
+          </div>
+          <div className={`nav-option ${areNavLinksVisible ? 'open' : ''}`}>
+            <a href="#">About</a>
+          </div>
+          <div className={`nav-option ${areNavLinksVisible ? 'open' : ''}`}>
+            <div id="login-btn"><p>Login</p></div>
+          </div>
         </div>
       }
       <div id="hamburger-icon" className={areNavLinksVisible ? 'open' : ''} onClick={handleHamburgerClick}>
